ec131f8b4959ce2d5400b999420abd1bf7fbd1aa
idlannaz <idlanmohd01@gmail.com>
2024-08-12 11:57:36 +0800

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import Products from './Products/Products'
+import { BrowseRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
   return (
     <div>
-      <Products />
+      <Router>
+        <Navigationbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<About />} />
+          <Route path="/" element={<Products />} />
+          <Route path="/" element={<Contact />} />
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   )
 }
